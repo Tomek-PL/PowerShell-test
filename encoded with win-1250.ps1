de2be160daddd533e202d3af974a9cff235a67bf
Tomek-PL <46404284+Tomek-PL@users.noreply.github.com>
2022-01-12 20:44:13 +0100

    Add files via upload

--- encoded with win-1250.ps1
+++ encoded with win-1250.ps1
@@ -1,3 +1,4 @@
 # This file is encoded with "Central European" Windows 1250:
+Write-Host "Za¿ó³ci³o gêsi¹ jaŸñ"
 Write-Host "(Naciœnij dowolny klawisz, aby zakoñczyæ)" -NoNewline
 [void][System.Console]::ReadKey($true)
